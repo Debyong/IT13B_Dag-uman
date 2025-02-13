a5f79bbecfc81fbeefa92cb90f5fadf5447ef1b5
Debyong <davejohndaguman7@gmail.com>
2025-02-13 11:48:05 +0800

    added activity 1

---
+++ Dag-uman_IT13B_activities/Lab_act1_arithmetics.java
@@ -0,0 +1,20 @@
+package Prelim;
+
+
+public class Lab_act1_arithmetics {
+    public static void main (String[] args){
+        int x = 10;
+        int y = 4;
+        int z = 6;
+         
+        
+        System.out.println(x + " * " + y + " + " + z + " = " + (x * y + z));
+        System.out.println(x + "-" + y + "-" + z +  "="  + (x - y - z));
+        System.out.println(x + " * + = " + (x + y + z));
+        System.out.println("x / y = " + (x / y)); // Integer division
+        System.out.println("x % y = " + (x % y)); // Modulus operation
+    }
+}
+
+    
+
