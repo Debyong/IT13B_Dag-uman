9e45b42235e5599b4644791be6612715b80ca129
Debyong <davejohndaguman7@gmail.com>
2025-04-04 12:16:38 +0800

    Add files via upload
    single_array

---
+++ 13B_DagUman/src/Midterm/SINGLE_ARRAY.java
@@ -0,0 +1,25 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class SINGLE_ARRAY {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+
+String[] array = {"A", "B", "C", "D", "E", "F", "G"};
+
+for (String letter : array) {
+   System.out.print(letter + " , ");
+}
+   }
+}  
+
